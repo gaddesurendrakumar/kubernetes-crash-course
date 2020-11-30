0fd8d2ad954f32fd2cc6410b78f849edfc5a0b2f
sgadde <gaddesurendrakumar@gmail.com>
2020-11-30 18:30:57 +0530

    Modified to deploy container to k8s

--- 02-todo-web-application-h2/src/main/resources/data.sql
+++ 02-todo-web-application-h2/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into TODO
-values(10001, 'Learn Spring Boot', false, sysdate(), 'in28minutes');
+values(10001, 'Learn Spring Boot', false, sysdate(), 'admin');
 insert into TODO
-values(10002, 'Learn Angular JS', false, sysdate(), 'in28minutes');
+values(10002, 'Learn Angular JS', false, sysdate(), 'admin');
 insert into TODO
-values(10003, 'Learn to Dance', false, sysdate(), 'in28minutes');
+values(10003, 'Learn to Dance', false, sysdate(), 'admin');
